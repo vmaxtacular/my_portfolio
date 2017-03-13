a61c789668454d222ddbe631471774b623661b22
Veroncia Maxwell <maxwellveronica1@gmail.com>
2017-03-13 18:33:02 +0800

    Added scaffold to user_mailers'

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
-  layout 'mailer'
+  
+  def welcome_email(user)
+  	@user = Users
+  	@url = 'https://evening-brushlands-21222.herokuapp.com/user/sign_up'
+  	mail(to: @admin.email, subject: 'Welcome to your portfolio Veronica!')
+  end
 end
